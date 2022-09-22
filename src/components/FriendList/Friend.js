acbfbff13a03b3e022e4c12a1da7a1461aea893c
AndriyKozh <98039703+AndriyKozh@users.noreply.github.com>
2022-09-22 22:23:20 +0300

    1.4

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -5,7 +5,7 @@ function Friend(props) {
   return (
     <div>
       {isOnline ? <div>red</div> : <div>green</div>}
-      <img src={avatar} alt="Photo" />
+      <img src={avatar} alt="User avatar" width="48" />
       <h2> {name}</h2>
     </div>
   );
